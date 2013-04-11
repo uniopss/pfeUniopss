2c4364daa82980855225b75bcded4f15f682fc10
Francis <webmaster@uniopss.asso.fr>
2013-04-11 18:48:38 +0200

    creation nouveau

---
+++ src/java/org/nextime/ion/backoffice/form/FrancisForm.java
@@ -0,0 +1,20 @@
+package org.nextime.ion.backoffice.form;
+
+public class FrancisForm {
+
+	private String prenom;
+	private String nom;
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+}
